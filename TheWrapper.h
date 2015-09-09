56dea545e6548c0120cbabc9b39855a07fa8a447
Guo Yu <canopus4u@outlook.com>
2015-09-09 18:57:13 +0800

    add Foundation.h and remove some interfaces

--- TheWrapper.h
+++ TheWrapper.h
@@ -2,18 +2,16 @@
 //  TheWrapper.h
 //
 //  Created by Tomer Shiri on 1/10/13.
+//  Modified by deput on 9/9/15
 //  Copyright (c) 2013 Tomer Shiri. All rights reserved.
 //
+#import <Foundation/Foundation.h>
 
 @interface TheWrapper : NSObject
 
-+(void) addWrapperto:(id<NSObject>) target andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,id firstArg, ...)) preRunblock;
-+(void) addWrapperto:(id<NSObject>) target andSelector:(SEL) selector withPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,id firstArg, ...)) postRunBlock;
-+(void) addWrapperto:(id<NSObject>) target andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,id firstArg, ...)) preRunblock andPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,id firstArg, ...)) postRunBlock;
-
-+(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,id firstArg, ...)) preRunblock;
-+(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,id firstArg, ...)) postRunBlock;
-+(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,id firstArg, ...)) preRunblock andPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,id firstArg, ...)) postRunBlock;
++(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,NSArray* args)) preRunblock;
++(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,NSArray* args)) postRunBlock;
++(void) addWrappertoClass:(Class) clazz andSelector:(SEL) selector withPreRunBlock:(void (^)(id<NSObject> zelf,NSArray* args)) preRunblock andPostRunBlock:(id (^)(id<NSObject> zelf, id functionReturnValue,NSArray* args)) postRunBlock;
 
 +(void) removeWrapperFrom:(id<NSObject>) target andSelector:(SEL) selector;
 +(void) removeWrapperFromClass:(Class) clazz andSelector:(SEL) selector;
